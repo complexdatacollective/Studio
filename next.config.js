1677804f81f80047f3159b16054d3d5ae7996e98
Joshua Melville <joshua.melville@northwestern.edu>
2023-06-21 18:26:29 +0200

    bootstrap project from fresh create-next-app

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    reactRoot: true,
-    concurrentFeatures: true,
-    serverComponents: true,
-    runtime: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- pages/about.tsx
+++
@@ -1,5 +0,0 @@
-function About() {
-  return <div>About my balls</div>
-}
-
-export default About
