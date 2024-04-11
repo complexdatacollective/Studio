6c5b8cb2815b407cf594437472775afa8b8c72e1
mrkarimoff <mrkarimoff99@gmail.com>
2024-04-11 17:17:01 +0500

    Refactor OrganizationSwitcher component to use useMemo for selectedOrg state

--- components/OrganizationSwitcher.tsx
+++ components/OrganizationSwitcher.tsx
@@ -10,15 +10,17 @@ import { useQueryWithAuth } from '~/hooks/useAuth';
 import { useQuery } from 'convex/react';
 import { api } from '~/convex/_generated/api';
 import { useRouter, useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useMemo } from 'react';
 
 export function OrganizationSwitcher() {
   const params = useParams();
   const router = useRouter();
 
-  const [selectedOrg, setSelectedOrg] = useState(
-    params && typeof params.organization === 'string' ? params.organization : ''
-  );
+  const selectedOrg = useMemo(() => {
+    return params && typeof params.organization === 'string'
+      ? params.organization
+      : '';
+  }, [params.organization]);
 
   const user = useQueryWithAuth(api.users.get, {});
   if (!user) {
@@ -35,13 +37,13 @@ export function OrganizationSwitcher() {
   };
 
   return (
-    <Select onValueChange={selectOrg} defaultValue={selectedOrg}>
+    <Select onValueChange={selectOrg} value={selectedOrg}>
       <SelectTrigger className='w-64'>
         <SelectValue placeholder='Select an Organization' />
       </SelectTrigger>
       <SelectContent>
         {organizations?.map((org) => (
-          <SelectItem key={org?._id} value={org?.slug}>
+          <SelectItem key={org?._id} value={org?.slug!}>
             {org?.name}
           </SelectItem>
         ))}
