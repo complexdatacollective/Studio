e1bbe36219a76643558b3ef163952ff83a9586d4
Caden Buckhalt <halaeb@gmail.com>
2024-06-06 14:00:35 -0700

    remaining interview route group and layout

--- app/[org]/layout.tsx
+++ app/[org]/layout.tsx
@@ -8,14 +8,14 @@ type OrganizationLayoutProps = {
 const OrganizationLayout = ({ children, params }: OrganizationLayoutProps) => {
   const { org: orgSlug } = params;
   return (
-    <>
+    <main>
       <div className="flex flex-row space-x-4 px-4 pt-4 text-lg">
         <Link href={`/`}>Studio</Link>
         <Link href={`/${orgSlug}`}>Org Dashboard</Link>
         <Link href={`/${orgSlug}/settings`}>Org Settings</Link>
       </div>
       {children}
-    </>
+    </main>
   );
 };
 
