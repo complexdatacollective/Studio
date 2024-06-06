414df11f7627b9fe945f607c7abbe2fffdc060c9
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-06 15:47:58 +0200

    fix knip configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
 // cannot use import aliases here with knip. see https://knip.dev/reference/known-issues#path-aliases-in-config-files
 import { defineConfig } from "drizzle-kit";
-import dotenv from "dotenv";
-dotenv.config();
+import { env } from "./env";
 
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   dialect: "postgresql",
   out: "./drizzle", // where to output the migration files
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? "",
+    url: env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
