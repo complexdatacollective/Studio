cacf057d44c7a44c9a24cbd5f59f5793ad525b6f
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-10 17:13:42 +0200

    finish rebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import { getOrganizations } from '~/server/queries/organizations';
 import CreateOrgForm from '~/app/[org]/_components/CreateOrgForm';
-<<<<<<< HEAD
 import { requirePageAuth } from '~/lib/auth';
-import SignOutBtn from './_components/SignOutBtn';
-=======
-import { requirePageAuth } from '~/utils/auth';
 import SignOutBtn from './_components/SignOutBtn';
 import Link from 'next/link';
 import { routes } from '~/lib/routes';
->>>>>>> aaafd9b (Implement type safe routing with next-safe-navigation and adjust the code for most of the pages)
 
 export default async function Home() {
   await requirePageAuth();
@@ -21,11 +16,10 @@ export default async function Home() {
       <h2 className="pb-4 text-2xl">All Organizations</h2>
       <div className="flex flex-col text-blue-700 underline">
         {allOrgs.map((org) => (
-<<<<<<< HEAD
-          <a key={org.public_id} href={`/${org.slug}`}>
-=======
-          <Link key={org.id} href={routes.orgDashboard({ org: org.slug })}>
->>>>>>> aaafd9b (Implement type safe routing with next-safe-navigation and adjust the code for most of the pages)
+          <Link
+            key={org.public_id}
+            href={routes.orgDashboard({ org: org.slug })}
+          >
             {org.name}
           </Link>
         ))}
