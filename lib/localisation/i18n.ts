c889501c4d2386e48c28e07fc75c279364fd34e9
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-11 11:28:39 +0200

    update type

--- lib/localisation/i18n.ts
+++ lib/localisation/i18n.ts
@@ -1,10 +1,11 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { SUPPORTED_LOCALES } from './locales';
+import { isIn } from '../utils';
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!SUPPORTED_LOCALES.includes(locale)) notFound();
+  if (!isIn(SUPPORTED_LOCALES, locale)) notFound();
 
   return {
     //Todo: fix linting error here later
