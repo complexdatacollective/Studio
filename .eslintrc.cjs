d5e334ba531867f7850b39efad930c7052d6eaf5
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-06-11 17:08:17 +0500

    remove next config file from ignorePatterns in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,65 +1,64 @@
-const path = require("path");
+const path = require('path');
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
   overrides: [
     {
       extends: [
-        "plugin:@typescript-eslint/stylistic-type-checked",
-        "plugin:@typescript-eslint/recommended-type-checked",
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:@typescript-eslint/recommended-type-checked',
       ],
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
+        project: path.join(__dirname, 'tsconfig.json'),
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
+    project: path.join(__dirname, 'tsconfig.json'),
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/stylistic",
-    "plugin:@typescript-eslint/recommended",
-    "next/core-web-vitals",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/stylistic',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
   ],
   ignorePatterns: [
-    "node_modules",
-    "*.stories.*",
-    "*.test.*",
-    "public",
-    ".eslintrc.cjs",
-    "next.config.mjs",
+    'node_modules',
+    '*.stories.*',
+    '*.test.*',
+    'public',
+    '.eslintrc.cjs',
   ],
   rules: {
-    "@next/next/no-img-element": "off",
-    "import/no-anonymous-default-export": "off",
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    "no-process-env": "error",
-    "no-console": "error",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
+    '@next/next/no-img-element': 'off',
+    'import/no-anonymous-default-export': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    'no-process-env': 'error',
+    'no-console': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
       {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
       },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        argsIgnorePattern: "^_",
+        argsIgnorePattern: '^_',
       },
     ],
-    "@typescript-eslint/no-misused-promises": [
-      "error",
+    '@typescript-eslint/no-misused-promises': [
+      'error',
       {
         checksVoidReturn: false,
       },
     ],
-    "no-unreachable": "error",
+    'no-unreachable': 'error',
   },
 };
 
