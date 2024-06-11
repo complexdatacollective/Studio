f452171af4a981cdae8a746f8b2c68fff0a65808
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-11 14:04:04 +0200

    remove eslint ignore from i18n

--- lib/localisation/i18n.ts
+++ lib/localisation/i18n.ts
@@ -1,14 +1,19 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { SUPPORTED_LOCALES } from './locales';
+import { type AbstractIntlMessages } from 'next-intl';
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!SUPPORTED_LOCALES.includes(locale)) notFound();
 
+  const messages = (
+    (await import(`./messages/${locale}.json`)) as {
+      default: AbstractIntlMessages | undefined;
+    }
+  ).default;
+
   return {
-    //Todo: fix linting error here later
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-    messages: (await import(`./messages/${locale}.json`)).default,
+    messages,
   };
 });
