34470ea8a37fb068b6391dfc3539fe70b0bb8e0d
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-06-11 18:22:39 +0500

    add translations to the signIn, signUp and home page

--- app/[locale]/signin/page.tsx
+++ app/[locale]/signin/page.tsx
@@ -9,9 +9,11 @@ import { validateRequest } from '~/lib/auth';
 import SignInForm from './_components/SignInForm';
 import { routes } from '~/lib/routes';
 import { Link, redirect } from '~/lib/localisation/navigation';
+import { getTranslations } from 'next-intl/server';
 
 export default async function Page() {
   const { session, user } = await validateRequest();
+  const t = await getTranslations('SignIn');
 
   if (session && user) {
     // If the user is already signed in, redirect to the home page
@@ -22,11 +24,11 @@ export default async function Page() {
     <div className="grid h-[100vh] w-full items-center justify-center gap-1.5">
       <Card className="m-3 w-[28rem]">
         <CardHeader>
-          <CardTitle>Sign in to Studio</CardTitle>
+          <CardTitle>{t('cardTitle')}</CardTitle>
           <CardDescription>
-            Don&apos;t have an account?{' '}
+            {t('cardDescription')}{' '}
             <Link className="text-blue-400 underline" href={routes.signUp()}>
-              Sign Up
+              {t('linkText')}
             </Link>
           </CardDescription>
         </CardHeader>
