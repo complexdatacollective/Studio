2a648a2694e38b22260e3f7ff7b5ee745a22689b
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-11 13:50:48 +0200

    remove eslint ignore from db

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -3,22 +3,12 @@ import * as schema from './schema';
 import { type PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-declare global {
-  // Todo: figure out how to fix linting here
-  // eslint-disable-next-line no-var
-  var db: PostgresJsDatabase<typeof schema> | undefined;
-}
+const globalForDb = globalThis as unknown as {
+  db: PostgresJsDatabase<typeof schema> | undefined;
+};
 
-let db: PostgresJsDatabase<typeof schema>;
 const pg: ReturnType<typeof postgres> = postgres(env.DATABASE_URL);
 
-if (env.NODE_ENV === 'production') {
-  db = drizzle(pg, { schema });
-} else {
-  if (!global.db) {
-    global.db = drizzle(pg, { schema });
-  }
-  db = global.db;
-}
+export const db = globalForDb.db ?? drizzle(pg, { schema });
 
-export { db };
+if (env.NODE_ENV !== 'production') globalForDb.db = db;
