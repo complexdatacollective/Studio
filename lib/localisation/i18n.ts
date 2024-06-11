b04b499ddf3995bba217c6e26d985cafe6c87231
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-11 11:33:04 +0200

    add ts-reset

--- lib/localisation/i18n.ts
+++ lib/localisation/i18n.ts
@@ -1,11 +1,10 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 import { SUPPORTED_LOCALES } from './locales';
-import { isIn } from '../utils';
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!isIn(SUPPORTED_LOCALES, locale)) notFound();
+  if (!SUPPORTED_LOCALES.includes(locale)) notFound();
 
   return {
     //Todo: fix linting error here later
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,11 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-/**
- * TS Util that allows array.includes to be used as a type guard
- * with read only arrays ('as const' arrays).
- */
-export function isIn<T>(values: readonly T[], x: any): x is T {
-  return values.includes(x);
-}
