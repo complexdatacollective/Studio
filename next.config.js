82cfdff7d8798b6e1a8051b38bb1a9a504b5d0a7
Joshua Melville <joshua.melville@northwestern.edu>
2024-06-20 13:48:10 +0200

    ignore eslint during build - we have separate CI task for this

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@node-rs/argon2'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default withNextIntl(nextConfig);
