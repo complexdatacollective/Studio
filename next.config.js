5a80aa6391c37a6243b6683d131978b020e59f6a
Caden Buckhalt <halaeb@gmail.com>
2024-06-26 12:29:21 -0700

    test adding back in experimental config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin(
-  './lib/localisation/i18n.ts'
-);
+const withNextIntl = createNextIntlPlugin('./lib/localisation/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Todo: remove this config once we upgrade to Next 15
+  experimental: {
+    serverComponentsExternalPackages: ['@node-rs/argon2'],
+  },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
 };
 
 export default withNextIntl(nextConfig);
