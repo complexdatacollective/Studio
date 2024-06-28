4816dbd6ee7488014f4ee78997d32600dfb1f240
Caden Buckhalt <halaeb@gmail.com>
2024-06-28 12:19:23 -0700

    move authed action buttons to test page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,10 +5,6 @@ import SignOutBtn from '../_components/SignOutBtn';
 import { routes } from '~/lib/routes';
 import { getTranslations } from 'next-intl/server';
 import { Link } from '~/lib/localisation/navigation';
-import {
-  TestAuthedActionButton,
-  TestAdminActionButton,
-} from './_components/TestAuthedActionButton';
 
 export default async function Home() {
   await requirePageAuth();
@@ -32,8 +28,6 @@ export default async function Home() {
       </div>
 
       <SignOutBtn />
-      <TestAuthedActionButton />
-      <TestAdminActionButton />
     </main>
   );
 }
