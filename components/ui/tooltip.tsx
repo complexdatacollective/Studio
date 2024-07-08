d612c583aadf0979476d0e6d608fa489e4ec66ee
Caden Buckhalt <halaeb@gmail.com>
2024-07-08 12:14:49 -0700

    add tooltip arrow

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -6,11 +6,8 @@ import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import { cn } from '~/lib/utils';
 
 const TooltipProvider = TooltipPrimitive.Provider;
-
 const Tooltip = TooltipPrimitive.Root;
-
 const TooltipTrigger = TooltipPrimitive.Trigger;
-
 const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
@@ -23,7 +20,12 @@ const TooltipContent = React.forwardRef<
       className,
     )}
     {...props}
-  />
+  >
+    <div className="flex">
+      <TooltipPrimitive.Arrow className="fill-accent" />
+      <div className="ml-1">{props.children}</div>
+    </div>
+  </TooltipPrimitive.Content>
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
