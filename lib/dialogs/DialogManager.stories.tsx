6ee892a9e39eb14c3ac80d806ec54e7feaf91990
Mirfayz Karimoff <mrkarimoff99@gmail.com>
2024-07-09 19:07:27 +0500

    refactor dialog and dialogmanager stories

--- lib/dialogs/DialogManager.stories.tsx
+++ lib/dialogs/DialogManager.stories.tsx
@@ -62,13 +62,34 @@ export const DialogManagerComponent: Story = {
         ),
         confirmLabel: 'Got it',
       });
+      openDialog({
+        type: 'Error',
+        title: 'Oops, something went wrong',
+        content: 'We encountered an unexpected error. Please try again later.',
+        error: {
+          name: 'TypeError',
+          message: 'Cannot read property `length` of undefined',
+          stack: `TypeError: Cannot read property length of undefined at
+            Object.handleClick (/app/components/Button.js:21:30) at
+            HTMLUnknownElement.callCallback (react-dom.development.js:4164:14) at
+            Object.invokeGuardedCallbackDev (react-dom.development.js:4213:16) at
+            invokeGuardedCallback (react-dom.development.js:4277:31) at
+            invokeGuardedCallbackAndCatchFirstError
+            (react-dom.development.js:4291:25) at executeDispatch
+            (react-dom.development.js:9041:3) at processDispatchQueueItemsInOrder
+            (react-dom.development.js:9073:7) at processDispatchQueue
+            (react-dom.development.js:9086:7) at dispatchEventsForPlugins
+            (react-dom.development.js:9097:7) at react-dom.development.js:9306:7`,
+        },
+        confirmLabel: 'OK',
+      });
     };
     return (
       <>
         {/* Button is not part of the DialogManager component,
             just putting there to showcase what the component renders
          */}
-        <Button onClick={createDialog}>Show Dialog</Button>
+        <Button onClick={createDialog}>Show Dialogs</Button>
 
         <AnimatePresence>
           {dialogs.map((dialog, index) => (
