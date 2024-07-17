a5c3f97119850e8d4ce0e3ab35e2974c569743c9
Caden Buckhalt <halaeb@gmail.com>
2024-07-17 14:36:08 -0700

    conditioanlly rendered popover text based on stage type

--- app/[locale]/(interview)/interview/_components/StageShell.tsx
+++ app/[locale]/(interview)/interview/_components/StageShell.tsx
@@ -6,19 +6,31 @@ import type { Stage } from '@prisma/client';
 // This is the shell for the interview stage. It contains the main task area and interview task hints/participant instructions.
 export default function StageShell({ stage }: { stage: Stage }) {
   const tTooltip = useTranslations('ExampleTooltip');
-  const tPopover = useTranslations('ExamplePopover');
+  const tInformationPopover = useTranslations('InformationPopover');
+  const tNameGeneratorPopover = useTranslations('NameGeneratorPopover');
+
+  const popoverContent =
+    stage.type === 'NameGenerator'
+      ? {
+          title: tNameGeneratorPopover('title'),
+          hint: tNameGeneratorPopover('hint'),
+        }
+      : {
+          title: tInformationPopover('title'),
+          hint: tInformationPopover('hint'),
+        };
+
   return (
     <div className="space-y-4 p-8">
       <h1 className="mb-4 text-2xl font-bold">Interview Stage {stage.id}</h1>
-      <p>This is the main task area</p>
-      <PopoverHint title={tPopover('title')} hint={tPopover('hint')} />
+      <h1 className="mb-4 text-2xl font-bold">{stage.type}</h1>
+      <PopoverHint title={popoverContent.title} hint={popoverContent.hint} />
       {/* example stage components */}
       <TooltipHint hint={tTooltip('hint')}>
         <div className="flex h-48 w-48 items-center justify-center rounded-full bg-neon-coral-dark text-center text-white">
           Example node
         </div>
       </TooltipHint>
-      {JSON.stringify(stage)}
     </div>
   );
 }
