10014fce0dbe88993eb9d1828a9f4e950fdb5ca0
Caden Buckhalt <halaeb@gmail.com>
2024-07-17 09:07:11 -0700

    correctly merge next config

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const withNextIntl = createNextIntlPlugin('./lib/localisation/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Todo: remove this config once we upgrade to Next 15
-  experimental: {
-    serverComponentsExternalPackages: ['@node-rs/argon2'],
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
