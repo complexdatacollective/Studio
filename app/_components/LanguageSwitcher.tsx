a172fba0cb2f2119672ea8c1a01dca3937f486cd
Caden Buckhalt <halaeb@gmail.com>
2024-07-18 14:17:47 -0700

    add all ar translations

--- app/_components/LanguageSwitcher.tsx
+++ app/_components/LanguageSwitcher.tsx
@@ -8,10 +8,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from '~/components/ui/select';
-import { SUPPORTED_LOCALES } from '~/lib/localisation/locales';
+import { type Locales, SUPPORTED_LOCALES } from '~/lib/localisation/locales';
 import { usePathname, useRouter } from '~/lib/localisation/navigation';
-
-type Locales = (typeof SUPPORTED_LOCALES)[number];
 
 const LanguageSwitcher = () => {
   const currentLocale = useLocale() as unknown as Locales;
@@ -26,11 +24,11 @@ const LanguageSwitcher = () => {
   return (
     <Select onValueChange={handleLanguageChange}>
       <SelectTrigger
-        className={`w-[200px] space-x-1 bg-white text-xs hover:bg-stone-100 sm:text-sm`}
+        className={`hover:bg-stone-100 w-[200px] space-x-1 bg-white text-xs sm:text-sm`}
       >
         <SelectValue placeholder={t(currentLocale)} />
       </SelectTrigger>
-      <SelectContent className="bg-white dark:bg-slate-700">
+      <SelectContent className="dark:bg-slate-700 bg-white">
         {SUPPORTED_LOCALES.map((locale) => (
           <SelectItem key={locale} value={locale}>
             {t(locale)}
