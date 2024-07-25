0208eacdd8ff789cd73ba33073229320f18896a4
Caden Buckhalt <halaeb@gmail.com>
2024-07-25 12:54:35 -0500

    rm: outdated comment bc we are using SUPPORTED_LOCALES in the matcher in nextIntlMiddleware.ts now

--- lib/localisation/locales.ts
+++ lib/localisation/locales.ts
@@ -1,4 +1,3 @@
-// ! Remember to modify the middleware.ts file as you add or remove locales here
 export const SUPPORTED_LOCALES = ['en', 'es', 'ar'] as const;
 
 export type Locale = (typeof SUPPORTED_LOCALES)[number];
