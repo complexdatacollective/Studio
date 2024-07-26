600c58092239b37ddd5b925153a8ecd4040e4d88
Caden Buckhalt <halaeb@gmail.com>
2024-07-26 16:21:07 -0500

    refactor: replace middleware with ratelimit within route handler

--- env.ts
+++ env.ts
@@ -6,6 +6,10 @@ export const env = createEnv({
   server: {
     POSTGRES_PRISMA_URL: z.string(),
     POSTGRES_URL_NON_POOLING: z.string(),
+    KV_REST_API_READ_ONLY_TOKEN: z.string(),
+    KV_REST_API_TOKEN: z.string(),
+    KV_REST_API_URL: z.string(),
+    KV_URL: z.string(),
   },
   client: {},
   shared: {
@@ -17,6 +21,10 @@ export const env = createEnv({
     POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
     NODE_ENV: process.env.NODE_ENV,
+    KV_REST_API_READ_ONLY_TOKEN: process.env.KV_REST_API_READ_ONLY_TOKEN,
+    KV_REST_API_TOKEN: process.env.KV_REST_API_TOKEN,
+    KV_REST_API_URL: process.env.KV_REST_API_URL,
+    KV_URL: process.env.KV_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,8 @@
 import { middlewareStacker } from './lib/middleware/middlewareStacker';
 import nextIntlMiddleware from './lib/middleware/nextIntlMiddleware';
 import CSRFMiddleware from './lib/middleware/CSRFMiddleware';
-import rateLimit from './lib/middleware/rateLimit';
 
-export default middlewareStacker([
-  nextIntlMiddleware,
-  CSRFMiddleware,
-  rateLimit,
-]);
+export default middlewareStacker([nextIntlMiddleware, CSRFMiddleware]);
 
 export const config = {
   // Only run middleware at all on these paths:
