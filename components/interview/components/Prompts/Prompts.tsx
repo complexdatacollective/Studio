43a2a886c981a163b7bfed413bed51a965358bd0
Caden Buckhalt <halaeb@gmail.com>
2024-08-02 10:37:40 -0500

    rm animation from prompts

--- components/interview/components/Prompts/Prompts.tsx
+++ components/interview/components/Prompts/Prompts.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { AnimatePresence, motion } from 'framer-motion';
 import Prompt from './Prompt';
 import Pips from './Pips';
 
@@ -19,16 +18,15 @@ export default function Prompts({
   const currentIndex = prompts.findIndex(({ id }) => id === currentPromptId);
 
   return (
-    <motion.div className="flex w-full flex-col items-center justify-center text-balance pb-2 text-center leading-tight">
+    <div className="flex w-full flex-col items-center justify-center text-balance pb-5 text-center leading-tight">
       <Pips count={prompts.length} currentIndex={currentIndex} />
-      <AnimatePresence mode="wait" initial={false}>
-        {prompts.map(
-          ({ id, text }) =>
-            prompts[currentIndex]?.id === id && (
-              <Prompt key={id} id={id} text={text} />
-            ),
-        )}
-      </AnimatePresence>
-    </motion.div>
+
+      {prompts.map(
+        ({ id, text }) =>
+          prompts[currentIndex]?.id === id && (
+            <Prompt key={id} id={id} text={text} />
+          ),
+      )}
+    </div>
   );
 }
