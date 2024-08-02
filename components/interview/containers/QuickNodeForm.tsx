6100fbc37f8d807c40a43468ebe0f13b7c923de0
Caden Buckhalt <halaeb@gmail.com>
2024-08-02 14:17:01 -0500

    quick form and node

--- components/interview/containers/QuickNodeForm.tsx
+++ components/interview/containers/QuickNodeForm.tsx
@@ -2,16 +2,32 @@
 
 import { useState } from 'react';
 import ActionButton from '../components/ActionButton';
+import Node from '../components/Node';
 
 export default function QuickNodeForm() {
   const [showForm, setShowForm] = useState(false);
 
   return (
-    <div className="">
-      {showForm && <div>Form</div>}
-      <div className="absolute bottom-2 right-2 mr-8">
-        <ActionButton onClick={() => setShowForm(!showForm)} />
-      </div>
+    <div className="absolute bottom-4 right-2 flex items-center">
+      {showForm && (
+        <div className="relative flex items-center">
+          <form>
+            <input
+              type="text"
+              placeholder="Type a label and press enter..."
+              className="rounded-full px-6 py-4 text-lg font-semibold"
+            />
+          </form>
+          <div onClick={() => setShowForm(false)}>
+            <Node label="" />
+          </div>
+        </div>
+      )}
+      {!showForm && (
+        <div className="mr-8">
+          <ActionButton onClick={() => setShowForm(true)} />
+        </div>
+      )}
     </div>
   );
 }
