14cf30b475448213557b82cd7446cb5ac049e065
Joshua Melville <joshua.melville@northwestern.edu>
2024-08-07 16:14:11 +0200

    upgrade storybook and initial setup for theming

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,8 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
-    '../(app|components)/**/*.mdx',
-    '../(app|components)/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../(app|components|stories)/**/*.mdx',
+    '../(app|components|stories)/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-viewport',
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,136 +2,81 @@ import { type Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './lib/interviewer/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./(app|lib|components)/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      // NC colors
-      'neon-coral': {
-        DEFAULT: 'hsl(var(--neon-coral) / <alpha-value>)',
-        dark: 'hsl(var(--neon-coral--dark) / <alpha-value>)',
-      },
-      'sea-green': {
-        DEFAULT: 'hsl(var(--sea-green) / <alpha-value>)',
-        dark: 'hsl(var(--sea-green--dark) / <alpha-value>)',
-      },
-      'slate-blue': {
-        DEFAULT: 'hsl(var(--slate-blue) / <alpha-value>)',
-        dark: 'hsl(var(--slate-blue--dark) / <alpha-value>)',
-      },
-      'navy-taupe': {
-        DEFAULT: 'hsl(var(--navy-taupe) / <alpha-value>)',
-        dark: 'hsl(var(--navy-taupe--dark) / <alpha-value>)',
-      },
-      'cyber-grape': {
-        DEFAULT: 'hsl(var(--cyber-grape) / <alpha-value>)',
-        dark: 'hsl(var(--cyber-grape--dark) / <alpha-value>)',
-      },
-      'mustard': {
-        DEFAULT: 'hsl(var(--mustard) / <alpha-value>)',
-        dark: 'hsl(var(--mustard--dark) / <alpha-value>)',
-      },
-      'rich-black': {
-        DEFAULT: 'hsl(var(--rich-black) / <alpha-value>)',
-        dark: 'hsl(var(--rich-black--dark) / <alpha-value>)',
-      },
-      'charcoal': {
-        DEFAULT: 'hsl(var(--charcoal) / <alpha-value>)',
-        dark: 'hsl(var(--charcoal--dark) / <alpha-value>)',
-      },
-      'platinum': {
-        DEFAULT: 'hsl(var(--platinum) / <alpha-value>)',
-        dark: 'hsl(var(--platinum--dark) / <alpha-value>)',
-      },
-      'sea-serpent': {
-        DEFAULT: 'hsl(var(--sea-serpent) / <alpha-value>)',
-        dark: 'hsl(var(--sea-serpent--dark) / <alpha-value>)',
-      },
-      'purple-pizazz': {
-        DEFAULT: 'hsl(var(--purple-pizazz) / <alpha-value>)',
-        dark: 'hsl(var(--purple-pizazz--dark) / <alpha-value>)',
-      },
-      'paradise-pink': {
-        DEFAULT: 'hsl(var(--paradise-pink) / <alpha-value>)',
-        dark: 'hsl(var(--paradise-pink--dark) / <alpha-value>)',
-      },
-      'cerulean-blue': {
-        DEFAULT: 'hsl(var(--cerulean-blue) / <alpha-value>)',
-        dark: 'hsl(var(--cerulean-blue--dark) / <alpha-value>)',
-      },
-      'kiwi': {
-        DEFAULT: 'hsl(var(--kiwi) / <alpha-value>)',
-        dark: 'hsl(var(--kiwi--dark) / <alpha-value>)',
-      },
-      'neon-carrot': {
-        DEFAULT: 'hsl(var(--neon-carrot) / <alpha-value>)',
-        dark: 'hsl(var(--neon-carrot--dark) / <alpha-value>)',
-      },
-      'barbie-pink': {
-        DEFAULT: 'hsl(var(--barbie-pink) / <alpha-value>)',
-        dark: 'hsl(var(--barbie-pink--dark) / <alpha-value>)',
-      },
-      'tomato': {
-        DEFAULT: 'hsl(var(--tomato) / <alpha-value>)',
-        dark: 'hsl(var(--tomato--dark) / <alpha-value>)',
-      },
-      'transparent': 'transparent',
-      'white': 'hsl(var(--white) / <alpha-value>)',
+      // ----------------------------
+      // Special one-off colors
+      // ----------------------------
 
-      'background': 'hsl(var(--background) / <alpha-value>)',
-      'foreground': 'hsl(var(--foreground) / <alpha-value>)',
+      transparent: 'transparent',
+      white: 'hsl(100 100% 100% / <alpha-value>)',
+      black: 'hsl(0 0% 0% / <alpha-value>)',
 
-      'primary': {
+      // ----------------------------
+      // Semantic colors
+
+      primary: {
         DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
         foreground: 'hsl(var(--primary-foreground) / <alpha-value>)',
       },
-      'secondary': {
+      secondary: {
         DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
         foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)',
       },
-      'destructive': {
+      destructive: {
         DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
         foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)',
       },
-      'success': {
+      success: {
         DEFAULT: 'hsl(var(--success) / <alpha-value>)',
         foreground: 'hsl(var(--success-foreground) / <alpha-value>)',
       },
-      'info': {
+      info: {
         DEFAULT: 'hsl(var(--info) / <alpha-value>)',
         foreground: 'hsl(var(--info-foreground) / <alpha-value>)',
       },
-      'muted': {
+      muted: {
         DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
         foreground: 'hsl(var(--muted-foreground) / <alpha-value>)',
       },
-      'accent': {
+      accent: {
         DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
         foreground: 'hsl(var(--accent-foreground) / <alpha-value>)',
       },
-      'popover': {
+      popover: {
         DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
         foreground: 'hsl(var(--popover-foreground) / <alpha-value>)',
       },
-      'card': {
+      card: {
         DEFAULT: 'hsl(var(--card) / <alpha-value>)',
         foreground: 'hsl(var(--card-foreground) / <alpha-value>)',
       },
-      'panel': {
+      panel: {
         DEFAULT: 'hsl(var(--panel) / <alpha-value>)',
         foreground: 'hsl(var(--foreground) / <alpha-value>)',
       },
-      'input': {
+      input: {
         DEFAULT: 'hsl(var(--input) / <alpha-value>)',
         foreground: 'hsl(var(--input-foreground) / <alpha-value>)',
       },
-      'border': 'hsl(var(--border) / <alpha-value>)',
-      'link': 'hsl(var(--link) / <alpha-value>)',
+      border: 'hsl(var(--border) / <alpha-value>)',
+      link: 'hsl(var(--link) / <alpha-value>)',
 
-      'focus': 'hsl(var(--focus) / <alpha-value>)',
+      focus: 'hsl(var(--focus) / <alpha-value>)',
+
+      // ----------------------------
+      // Sequences
+      // Used for situations where we want to show a sequence of colors, such
+      // as node types, categorical bins etc
+      // ----------------------------
+
+      nodeSequence: {
+        1: 'hsl(var(--node-sequence-1) / <alpha-value>)',
+        2: 'hsl(var(--node-sequence-2) / <alpha-value>)',
+        3: 'hsl(var(--node-sequence-3) / <alpha-value>)',
+        4: 'hsl(var(--node-sequence-4) / <alpha-value>)',
+      },
     },
     borderRadius: {
       ...defaultTheme.borderRadius,
