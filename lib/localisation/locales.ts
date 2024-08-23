49b3f8ff50b7b8ecc6b3f570af9a0f54f257bcd4
Joshua Melville <joshua.melville@northwestern.edu>
2024-08-23 13:21:47 +0200

    revert localisation schema

--- lib/localisation/locales.ts
+++ lib/localisation/locales.ts
@@ -1,15 +1,9 @@
-import { z } from 'zod';
+export const LOCALES_DICT = [
+  ['en', 'English'],
+  ['es', 'Español'],
+  ['ar', 'عربي'],
+] as const;
 
-export const LocalesDictSchema = z
-  .object({
-    en: z.literal('English'),
-    es: z.literal('Español'),
-    ar: z.literal('عربي'),
-  })
-  .strict();
-
-export const SUPPORTED_LOCALES = [...LocalesDictSchema.keyof().options];
-
-export type TSupportedLocales = typeof SUPPORTED_LOCALES;
+export const SUPPORTED_LOCALES = LOCALES_DICT.map(([code]) => code);
 
 export type Locale = (typeof SUPPORTED_LOCALES)[number];
--- lib/schemas/shared.ts
+++ lib/schemas/shared.ts
@@ -1,10 +1,6 @@
 import { z } from 'zod';
-import { LocalesDictSchema } from '../localisation/locales';
 
 // Utility for protocol keys that must support localisation.
 // TODO: currently extends app supported locales, but should
 // be extended to support all possible locales.
-export const LocalisedRecord = z.record(
-  z.enum([...LocalesDictSchema.keyof().options]),
-  z.string(),
-);
+export const LocalisedRecord = z.record(z.string(), z.string());
