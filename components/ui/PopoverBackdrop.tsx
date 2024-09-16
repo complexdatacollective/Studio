a4f87caac12c5063de083bc76679c166ed8fd30a
Joshua Melville <joshua.melville@northwestern.edu>
2024-09-16 11:31:21 +0200

    resolve framer motion related build errors

--- components/ui/PopoverBackdrop.tsx
+++ components/ui/PopoverBackdrop.tsx
@@ -28,7 +28,7 @@ const PopoverBackdrop = forwardRef(
     ref,
   ) => {
     return (
-      // @ts-expect-error has to do with 12.0.0-alpha release
+      // @ts-expect-error incompatibility between framer-motion 12.x and new react types
       <motion.div
         ref={ref}
         className={cn(backdropClasses, className)}
