4096acebef6f1a06a0c6996061cfacd88122cdba
Caden Buckhalt <halaeb@gmail.com>
2024-10-31 07:45:43 -0700

    use useEditorState hook for states

--- lib/block-editor/extensions/BubbleMenu/BubbleMenu.tsx
+++ lib/block-editor/extensions/BubbleMenu/BubbleMenu.tsx
@@ -1,10 +1,26 @@
-import { BubbleMenu as BaseBubbleMenu, type Editor } from '@tiptap/react';
+import {
+  BubbleMenu as BaseBubbleMenu,
+  useEditorState,
+  type Editor,
+} from '@tiptap/react';
 import { Bold, Italic, Link, Trash } from 'lucide-react';
 import { Button, type ButtonProps } from '~/components/Button';
 import Popover from '~/components/Popover';
 import { withTooltip } from '~/components/Tooltip';
 
 export const BubbleMenu = ({ editor }: { editor: Editor | null }) => {
+  const editorState = useEditorState({
+    editor,
+    selector: ({ editor }) => {
+      return {
+        isBold: !!editor?.isActive('bold'),
+        isItalic: !!editor?.isActive('italic'),
+        isLink: !!editor?.isActive('link'),
+        activeLink: editor?.getAttributes('link').href,
+      };
+    },
+  });
+
   if (!editor) {
     return null;
   }
@@ -37,23 +53,23 @@ export const BubbleMenu = ({ editor }: { editor: Editor | null }) => {
           <MenuButtonWithTooltip
             onClick={() => editor.chain().focus().toggleBold().run()}
             tooltipContent="Bold (Ctrl+B)"
-            variant={editor.isActive('bold') ? 'default' : 'text'}
+            variant={editorState?.isBold ? 'default' : 'text'}
           >
             <Bold />
           </MenuButtonWithTooltip>
           <MenuButtonWithTooltip
             onClick={() => editor.chain().focus().toggleItalic().run()}
             tooltipContent="Italic (Ctrl+I)"
-            variant={editor.isActive('italic') ? 'default' : 'text'} // maybe should be passed as a 'selected' prop so that the variant can be standardized
+            variant={editorState?.isItalic ? 'default' : 'text'} // maybe should be passed as a 'selected' prop so that the variant can be standardized
           >
             <Italic />
           </MenuButtonWithTooltip>
           <Popover
             content={
-              editor.getAttributes('link').href ? (
+              editorState?.activeLink ? (
                 <div className="flex items-center gap-1 pr-8">
-                  <a href={editor.getAttributes('link').href as string}>
-                    {editor.getAttributes('link').href}
+                  <a href={editorState.activeLink as string}>
+                    {editorState.activeLink}
                   </a>
                   <Button variant="text" size="xs" onClick={handleLinkRemove}>
                     <Trash />
@@ -79,7 +95,7 @@ export const BubbleMenu = ({ editor }: { editor: Editor | null }) => {
           >
             <MenuButtonWithTooltip
               tooltipContent="Set Link"
-              variant={editor.getAttributes('link').href ? 'default' : 'text'}
+              variant={editorState?.isLink ? 'default' : 'text'}
             >
               <Link />
             </MenuButtonWithTooltip>
