04c27dec300e582e58f98ce2f31d4cb0cd527a10
Caden Buckhalt <halaeb@gmail.com>
2024-11-14 14:19:20 -0800

    fix setting children variable required props
    only set vars inside current group

--- lib/block-editor/extensions/Group/utils.ts
+++ lib/block-editor/extensions/Group/utils.ts
@@ -2,19 +2,43 @@ import type { Editor } from '@tiptap/react';
 
 export function toggleGroupRequired(editor: Editor) {
   if (!editor) return;
-  const groupRequired = editor.getAttributes('group')?.groupRequired;
 
-  // Traverse variable nodes and update required attribute
-  editor.state.doc.descendants((node, pos) => {
-    if (node.type.name === 'variable') {
-      editor.view.dispatch(
-        editor.state.tr.setNodeMarkup(pos, undefined, {
-          ...node.attrs,
-          required: !groupRequired,
-        }),
-      );
+  // find the current group node
+  const { from, to } = editor.state.selection;
+  let groupPos: number | null = null;
+  let groupNode = null;
+
+  editor.state.doc.nodesBetween(from, to, (node, pos) => {
+    if (node.type.name === 'group' && !groupNode) {
+      groupPos = pos;
+      groupNode = node;
+      return false;
     }
     return true;
   });
+
+  if (!groupNode || groupPos === null) return;
+
+  const groupRequired = groupNode.attrs.groupRequired;
+  const groupNodeSize = groupNode.nodeSize;
+
+  // traverse nodes inside the group node and update the required attribute of variable nodes
+  editor.state.doc.nodesBetween(
+    groupPos,
+    groupPos + groupNodeSize,
+    (node, pos) => {
+      if (node.type.name === 'variable') {
+        editor.view.dispatch(
+          editor.state.tr.setNodeMarkup(pos, undefined, {
+            ...node.attrs,
+            required: !groupRequired,
+          }),
+        );
+      }
+      return true;
+    },
+  );
+
+  // update the group's groupRequired attribute
   editor.commands.updateAttributes('group', { groupRequired: !groupRequired });
 }
