baff7351e571a2df158306ca841f748e2eaa1462
Caden Buckhalt <halaeb@gmail.com>
2024-11-26 16:43:27 -0600

    show placeholder when inserting

--- components/block-editor/PlusMenu.tsx
+++ components/block-editor/PlusMenu.tsx
@@ -1,5 +1,6 @@
 import { type Editor } from '@tiptap/react';
 import { Plus } from 'lucide-react';
+import { useState } from 'react';
 import { createVariableNode } from '~/lib/block-editor/extensions/Variable/utils';
 import { type TiptapContent, contentMap } from '~/lib/block-editor/types';
 import devProtocol from '~/lib/db/sample-data/dev-protocol';
@@ -8,13 +9,22 @@ import { Button } from '../Button';
 import DropdownMenu from '../DropdownMenu';
 
 export default function PlusMenu({ editor }: { editor: Editor | null }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   if (!editor) return null;
   const { variables } = devProtocol;
 
   const handleCommand = (type: TiptapContent) => () => {
     const endPos = editor.state.doc.content.size;
-    editor.chain().focus().insertContentAt(endPos, contentMap[type]).run();
-    editor.view.focus();
+    editor
+      .chain()
+      .focus()
+      .insertContentAt(endPos, contentMap[type], {
+        updateSelection: true,
+      })
+      .run();
+
+    setIsOpen(false);
   };
 
   const addVariable = (variable: TVariableDefinition) => {
@@ -26,14 +36,19 @@ export default function PlusMenu({ editor }: { editor: Editor | null }) {
     });
     const transaction = editor.view.state.tr.insert(endPos, variableNode);
     editor.view.dispatch(transaction);
+    setIsOpen(false);
   };
 
   return (
-    <DropdownMenu.Root>
+    <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
       <DropdownMenu.Trigger>
-        <Button variant="outline" size="icon" color="primary">
-          <Plus />
-        </Button>
+        {isOpen ? (
+          <div className="h-12 w-full rounded-small border border-dashed border-primary" />
+        ) : (
+          <Button variant="outline" size="icon" color="primary">
+            <Plus />
+          </Button>
+        )}
       </DropdownMenu.Trigger>
       <DropdownMenu.Content>
         <DropdownMenu.Label>Add content</DropdownMenu.Label>
