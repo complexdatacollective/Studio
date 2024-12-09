6735aaa5c573171fa914c1412bb38ad5b5368cfa
Caden Buckhalt <halaeb@gmail.com>
2024-12-09 13:28:17 -0800

    linting fixes

--- lib/block-editor/extensions/Group/utils.ts
+++ lib/block-editor/extensions/Group/utils.ts
@@ -1,18 +1,22 @@
 import { type Node } from '@tiptap/pm/model';
 import type { Editor } from '@tiptap/react';
 
+type GroupNode = Node & { attrs: { groupRequired: boolean } };
+
 export function toggleGroupRequired(editor: Editor) {
   if (!editor) return;
 
   // find the current group node
   const { from, to } = editor.state.selection;
   let groupPos: number | null = null;
-  let groupNode: Node | null = null;
+  let groupNode: GroupNode | null = null;
 
   editor.state.doc.nodesBetween(from, to, (node, pos) => {
     if (node.type.name === 'group') {
       groupPos = pos;
-      groupNode = node;
+      if ('groupRequired' in node.attrs) {
+        groupNode = node as GroupNode;
+      }
       return false;
     }
     return true;
@@ -20,8 +24,8 @@ export function toggleGroupRequired(editor: Editor) {
 
   if (!groupNode || groupPos === null) return;
 
-  const groupRequired = groupNode.attrs.groupRequired;
-  const groupNodeSize = groupNode.nodeSize;
+  const groupRequired = (groupNode as GroupNode).attrs.groupRequired;
+  const groupNodeSize = (groupNode as GroupNode).nodeSize;
 
   // traverse nodes inside the group node and update the required attribute of variable nodes
   editor.state.doc.nodesBetween(
--- lib/block-editor/extensions/SlashCommand/types.ts
+++ lib/block-editor/extensions/SlashCommand/types.ts
@@ -1,25 +1,25 @@
-import { Editor } from '@tiptap/core'
+import { type Editor } from '@tiptap/core';
 
-import { icons } from 'lucide-react'
+import { type icons } from 'lucide-react';
 
-export interface Group {
-  name: string
-  title: string
-  commands: Command[]
-}
+export type Group = {
+  name: string;
+  title: string;
+  commands: Command[];
+};
 
-export interface Command {
-  name: string
-  label: string
-  description: string
-  aliases?: string[]
-  iconName: keyof typeof icons
-  action: (editor: Editor) => void
-  shouldBeHidden?: (editor: Editor) => boolean
-}
+export type Command = {
+  name: string;
+  label: string;
+  description: string;
+  aliases?: string[];
+  iconName: keyof typeof icons;
+  action: (editor: Editor) => void;
+  shouldBeHidden?: (editor: Editor) => boolean;
+};
 
-export interface MenuListProps {
-  editor: Editor
-  items: Group[]
-  command: (command: Command) => void
-}
+export type MenuListProps = {
+  editor: Editor;
+  items: Group[];
+  command: (command: Command) => void;
+};
